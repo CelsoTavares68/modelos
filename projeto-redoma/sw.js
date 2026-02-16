9ae8fd09b83c25329ef212a568c790b369682737
CelsoTavares68 <celsotavares1914@gmail.com>
2026-02-16 20:36:42 -0300

    Update sw.js

--- projeto-redoma/sw.js
+++ projeto-redoma/sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'agenda-redoma-v3';
+const cacheName = 'agenda-redoma-v4';
 const assets = ['./', './index.html', './style.css', './script.js', './manifest.json', './icon.png'];
 
 // Instala o service worker e guarda os arquivos no cache
