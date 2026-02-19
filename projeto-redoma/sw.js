eea8d016f63134d33824c032fdd9c04399487f4d
CelsoTavares68 <celsotavares1914@gmail.com>
2026-02-19 11:24:08 -0300

    Update sw.js

--- projeto-redoma/sw.js
+++ projeto-redoma/sw.js
@@ -1,4 +1,4 @@
-    const cacheName = 'agenda-redoma-v107';
+    const cacheName = 'agenda-redoma-v108';
 const assets = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
 self.addEventListener('install', e => {
