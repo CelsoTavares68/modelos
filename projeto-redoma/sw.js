23408dae54e630a16b0123b954353b0c596cd3d2
CelsoTavares68 <celsotavares1914@gmail.com>
2026-02-19 10:11:28 -0300

    ...

--- projeto-redoma/sw.js
+++ projeto-redoma/sw.js
@@ -1,30 +1,18 @@
- const cacheName = 'agenda-final-v100'; // Nome totalmente novo para forçar o reset
-const assets = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js?v=100',
-  './manifest.json'
-];
+ const cacheName = 'agenda-redoma-v101';
+const assets = ['./', './index.html', './style.css', './script.js?v=100', './manifest.json'];
 
 self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
-  );
+  self.skipWaiting(); 
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => key !== cacheName && caches.delete(key))
-    ))
-  );
-  return self.clients.claim();
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.map(key => key !== cacheName && caches.delete(key))
+  )));
+  return self.clients.claim(); 
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
