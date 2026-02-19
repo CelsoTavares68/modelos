1e9bbb4174d4721def6607a8f3673aa2ca45078f
CelsoTavares68 <celsotavares1914@gmail.com>
2026-02-19 09:30:08 -0300

    Update sw.js

--- projeto-redoma/sw.js
+++ projeto-redoma/sw.js
@@ -1,12 +1,23 @@
-const cacheName = 'agenda-redoma-v16';
-const assets = ['./', './index.html', './style.css', './script.js', './manifest.json', './icon.png'];
+ const cacheName = 'agenda-redoma-v14'; // Mudamos para v14
+const assets = ['./', './index.html', './style.css', './script.js?v=2', './manifest.json', './icon.png'];
 
-// Instala o service worker e guarda os arquivos no cache
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+  self.skipWaiting(); // Força o código novo a assumir o lugar do velho imediatamente
 });
 
-// Responde mesmo se estiver offline
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== cacheName) return caches.delete(key); // Apaga as sobras da v13
+      }));
+    })
+  );
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
