e93852db5b5b5a68270420593240b9417b30aa1d
CelsoTavares68 <celsotavares1914@gmail.com>
2026-03-19 20:29:34 -0300

    Update script.js

--- projeto-enduro/script.js
+++ projeto-enduro/script.js
@@ -325,14 +325,14 @@ function update() {
     if (gameTick % 250 === 0 && enemies.length < 100) {
         enemies.push({ 
             lane: (Math.random() - 0.5) * 1.8, z: 4000, v: 8.0, 
-            color: ["#F0F", "#0FF", "#0F0", "#FF0"][Math.floor(Math.random() * 4)],
+             color: ["#F0F", "#0FF", "#0F0", "#FF0", "#f47d28", "#a5a3a3" ][Math.floor(Math.random() * 6)],
             isOvertaken: false 
         });
     }
     if (gameTick % 250 === 80 && enemies.length < 100) {
         enemies.push({ 
             lane: (Math.random() - 0.5) * 1.8, z: 4000, v: 8.0, 
-            color: ["#F0F", "#0FF", "#0F0", "#FF0"][Math.floor(Math.random() * 4)],
+            color: ["#F0F", "#0FF", "#0F0", "#FF0", "#f47d28", "#a5a3a3"][Math.floor(Math.random() * 6)],
             isOvertaken: false 
         });
     }
