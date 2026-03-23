dd262ee7d6a2c2503ee102f50ca8faf21f8e1d23
CelsoTavares68 <celsotavares1914@gmail.com>
2026-03-23 11:09:14 -0300

    Update script.js

--- projeto-enduro-mobile/script.js
+++ projeto-enduro-mobile/script.js
@@ -491,7 +491,7 @@ function togglePause() {
         enemy.lastY = 200 + (p * 140); enemy.lastX = screenX; enemy.lastP = p;
     });
 
-    if (gameTick % 80 === 0 && enemies.length < 100) {
+    if (gameTick % 75 === 0 && enemies.length < 100) {
         enemies.push({ 
             lane: (Math.random() - 0.5) * 1.8, z: 4000, v: 5.0, 
             color: ["#F0F", "#0FF", "#0F0", "#FF0", "#f47d28", "#a5a3a3", "rgb(0, 26, 255)", "rgb(27, 104, 27)" ][Math.floor(Math.random() * 8)],
