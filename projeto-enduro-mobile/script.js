615006d34cdb8f9fdc958a0800be34fcb5efb6ba
CelsoTavares68 <celsotavares1914@gmail.com>
2026-03-23 10:18:21 -0300

    Update script.js

--- projeto-enduro-mobile/script.js
+++ projeto-enduro-mobile/script.js
@@ -31,8 +31,8 @@ let passDayBest = parseInt(localStorage.getItem('enduro_passDayBest')) || 0;    
 let passTotalOdo = parseInt(localStorage.getItem('enduro_passTotalOdo')) || 0;   // 3. Odômetro total de carros
 let passTotalBest = parseInt(localStorage.getItem('enduro_passTotalBest')) || 0; // 4. Recorde total histórico
 
-const maxSpeed = 18; 
-const STAGE_DURATION = 3300; 
+const maxSpeed = 19; 
+const STAGE_DURATION = 3000; 
 const DAY_DURATION = STAGE_DURATION * 9; 
 let currentTime = 0; 
 
