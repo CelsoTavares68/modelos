e5acd6c03af990faea10c83c4e1c3373c405116a
CelsoTavares68 <celsotavares1914@gmail.com>
2026-05-01 11:36:05 -0300

    Update script.js

--- projeto-enduro-mobile/script.js
+++ projeto-enduro-mobile/script.js
@@ -31,8 +31,8 @@ let passDayBest = parseInt(localStorage.getItem('enduro_passDayBest')) || 0;    
 let passTotalOdo = parseInt(localStorage.getItem('enduro_passTotalOdo')) || 0;   // 3. Odômetro total de carros
 let passTotalBest = parseInt(localStorage.getItem('enduro_passTotalBest')) || 0; // 4. Recorde total histórico
 
-const maxSpeed = 19; 
-const STAGE_DURATION = 1620; 
+const maxSpeed = 20; 
+const STAGE_DURATION = 1680; 
 const DAY_DURATION = STAGE_DURATION * 9; 
 let currentTime = 0; 
 
@@ -582,7 +582,7 @@ function update() {
         enemy.lastY = 200 + (p * 140); enemy.lastX = screenX; enemy.lastP = p;
     });
 
-    if (gameTick % 50 === 0 && enemies.length < 100) {
+    if (gameTick % 45 === 0 && enemies.length < 100) {
         enemies.push({ 
             lane: (Math.random() - 0.5) * 1.8, z: 4000, v: 2.5, 
             color: ["#F0F", "#0FF", "#0F0", "#FF0", "#f47d28", "#a5a3a3", "rgb(0, 26, 255)", "rgb(27, 104, 27)" ][Math.floor(Math.random() * 8)],
